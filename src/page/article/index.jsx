62549383f04ad760adce98c2c4548892213af6e5
GanWeiren <ganweiren@kuaifazs.com>
2021-10-08 17:35:08 +0800

    article page base

---
+++ src/page/article/index.jsx
@@ -0,0 +1,26 @@
+import styles from './index.module.scss'
+
+const Article = () => {
+    return (
+        <div className={styles.article}>
+            <div>
+                <div className={styles['article-header']}>
+                    This is poster
+                </div>
+                <article className={styles['article-body']}>
+                    This is article
+                </article>
+                <aside className={styles['article-comment']}>
+                    This is commite
+                </aside>
+            </div>
+
+            <aside className={styles['article-sidebar']}>
+                This article struct
+            </aside>
+
+        </div>
+    )
+}
+
+export default Article
--- src/page/gallery/index.jsx
+++ src/page/gallery/index.jsx
@@ -3,25 +3,25 @@ import './index.scss'
 import Carousel from '../../components/common/Carousel'
 
 const Gallery = () => {
-  return (
-    <div id="gallery">
-      <div className='content'>
-        <div className='carousel'>
-          <Carousel />
-        </div>
-        <div className='photo-album'>
-          {
-            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1].map( (n, index) => {
-              return (
-                <div className='photo' key={index}>
+    return (
+        <div id="gallery">
+            <div className='content'>
+                <div className='carousel'>
+                    <Carousel/>
                 </div>
-              )
-            })
-          }
+                <div className='photo-album'>
+                    {
+                        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((n, index) => {
+                            return (
+                                <div className='photo' key={index}>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Gallery
