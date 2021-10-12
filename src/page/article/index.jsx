9a74f01e3c6a0a91494ae6b3d119189a33e903d8
Tony-Jack2017 <815378559@qq.com>
2021-10-12 17:53:19 +0800

    article page poster

--- src/page/article/index.jsx
+++ src/page/article/index.jsx
@@ -1,23 +1,24 @@
 import styles from './index.module.scss'
 
+import Poster from "./components/poster";
+
 const Article = () => {
     return (
         <div className={styles.article}>
             <div>
                 <div className={styles['article-header']}>
-                    This is poster
+                    <Poster />
                 </div>
                 <article className={styles['article-body']}>
-                    This is article
+                    {/*This is article*/}
                 </article>
                 <aside className={styles['article-comment']}>
-                    This is commite
+                    {/*This is commite*/}
                 </aside>
             </div>
             <aside className={styles['article-sidebar']}>
                 This article struct
             </aside>
-
         </div>
     )
 }
