e591f053d0fd4c43a4d737e428c7ef07ebe1b438
Tony-Jack2017 <815378559@qq.com>
2021-10-18 07:43:57 +0800

    home work -- gallery

--- src/page/gallery/index.jsx
+++ src/page/gallery/index.jsx
@@ -1,19 +1,36 @@
-import './index.scss'
+import  styles from './index.module.scss'
 
 import Carousel from '../../components/common/Carousel'
+import photoTest from '../../asset/mock/photo-test.jpg'
 
 const Gallery = () => {
     return (
-        <div id="gallery">
-            <div className='content'>
-                <div className='carousel'>
+        <div id={styles.gallery}>
+            <div className={styles.content}>
+                <div className={styles.carousel}>
                     <Carousel/>
                 </div>
-                <div className='photo-album'>
+                <div className={styles['photo-album']}>
                     {
                         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((n, index) => {
                             return (
-                                <div className='photo' key={index}>
+                                <div className={styles.photo} key={index}>
+                                    <div className={styles['photo-poster']}>
+                                        <img src={photoTest} />
+                                    </div>
+                                    <div className={styles['photo-content']}>
+                                        <h2>Why Use React to build our website interface</h2>
+                                        <p>TomJerry</p>
+                                        <div className={styles['content-footer']}>
+                                            <div style={{display: 'flex'}}>
+                                                <div style={{marginRight: '10px'}}>18</div>
+                                                <div>comment</div>
+                                            </div>
+                                            <div className={styles['learn-more']}>
+                                                learn more >>>>>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             )
                         })
