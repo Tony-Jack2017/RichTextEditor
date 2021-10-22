560156a94033ad69a963204de2172195c6e516b0
Tony-Jack2017 <815378559@qq.com>
2021-10-22 18:05:16 +0800

    company work -- user setting base

--- src/common/request.js
+++ src/common/request.js
@@ -1,33 +1,33 @@
 import axios from "axios";
 
 // create an axios instance
-  const service = axios.create({
+const service = axios.create({
     baseURL: '', // url = base url + request url
     timeout: 3000 //request timeout
-  })
-  /**
-   * 请求拦截
-   */
-  service.interceptors.request.use(
+})
+/**
+ * 请求拦截
+ */
+service.interceptors.request.use(
     config => {
-      if(window.localStorage.token){
-        config.headers['Authorization'] = window.localStorage.token
-      }
-      return config
+        if (window.localStorage.token) {
+            config.headers['Authorization'] = window.localStorage.token
+        }
+        return config
     }
-  )
-  /**
-   * 响应拦截
-   */
-  service.interceptors.response.use(
+)
+/**
+ * 响应拦截
+ */
+service.interceptors.response.use(
     response => {
-      const res = response.data
-      if(res.code !== 200) {
-        alert(res.msg)
-      }else{
-        return res
-      }
+        const res = response.data
+        if (res.code !== 200) {
+            alert(res.msg)
+        } else {
+            return res
+        }
     }
-  )
+)
 
 export default service
--- src/page/profile/Setting/components/UserSetting/index.jsx
+++ src/page/profile/Setting/components/UserSetting/index.jsx
@@ -1,11 +1,70 @@
 import styles from './index.module.scss'
 
+import avatar from '../../../../../asset/mock/author_avatar.jpg'
+
 const UserSetting = () => {
-  return (
-    <div>
-      This is UserSetting
-    </div>
-  )
+    return (
+        <div>
+            <form id={styles['edit-form']}>
+                <div>
+                    <div className={styles['form-item']}>
+                        <div className={styles['form-item-label']}>
+                            <label>Username</label>
+                        </div>
+                        <div className={styles['form-item-input']}>
+                            <input type='text' placeholder="Enter you full name"/>
+                        </div>
+                    </div>
+                    <div className={styles['form-item']}>
+                        <div className={styles['form-item-label']}>
+                            <label>Avatar</label>
+                        </div>
+                        <div className={styles['form-item-input']}>
+                            <div className={styles.avatar}>
+                                {/*<img src={avatar} alt=""/>*/}
+                            </div>
+                            <input type="file" style={{display: 'none'}}/>
+                        </div>
+                    </div>
+                    <div className={styles['form-item']}>
+                        <div className={styles['form-item-label']}>
+                            <label>About you</label>
+                        </div>
+                        <div className={styles['form-item-input']}>
+                            <textarea style={{height: '200px'}} placeholder="I am bitter bricklayer，..." maxlength="200"/>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div className={styles['form-item']}>
+                        <div className={styles['form-item-label']}>
+                            <label>Email</label>
+                        </div>
+                        <div className={styles['form-item-input']}>
+                            <input type="email" placeholder="815378559@.xx.com"/>
+                        </div>
+                    </div>
+                    <div className={styles['form-item']}>
+                        <div className={styles['form-item-label']}>
+                            <label>Github</label>
+                        </div>
+                        <div className={styles['form-item-input']}>
+                            <input type="text" placeholder="https://github.com/TonyJack-2017"/>
+                        </div>
+                    </div>
+                    <div className={styles['form-item']}>
+                        <div className={styles['form-item-label']}>
+                            <label>Gitee</label>
+                        </div>
+                        <div className={styles['form-item-input']}>
+                            <input type="text" placeholder="https://gitee.com/tron_century0"/>
+                        </div>
+                    </div>
+                </div>
+
+            </form>
+        </div>
+    )
 }
 
 export default UserSetting
