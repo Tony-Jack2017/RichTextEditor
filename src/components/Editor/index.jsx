67aecf6c1408158c2edf50985036f4f35fce132c
Tony-Jack2017 <815378559@qq.com>
2021-11-16 17:51:21 +0800

    剩下没传的

--- src/components/Editor/index.jsx
+++ src/components/Editor/index.jsx
@@ -1,49 +1,44 @@
+import { useState, useCallback } from "react";
+import { Slate, Editable } from "slate-react";
 
-import { useState, useCallback } from 'react'
-import { Slate, Editable} from 'slate-react'
+import styles from "./index.module.scss";
 
-import styles from './index.module.scss'
-
-
-import Leaf from './components/Leaf'
-import CodeElement from '../../common/components/Node/CodeElement'
-import DefaultElement from '../../common/components/Node/DefaultElement'
+import Leaf from "./components/Leaf";
+import CodeElement from "../../common/components/Node/CodeElement";
+import DefaultElement from "../../common/components/Node/DefaultElement";
 
 const Editor = (props) => {
-
-
   const [value, setValue] = useState([
     {
-      type: 'paragraph',
-      children: [ { text: '这是我的文本'} ]
-  }
-  ])
+      type: "paragraph",
+      children: [{ text: "这是我的文本" }],
+    },
+  ]);
 
   const handleChange = (value) => {
-    setValue(value)
-  }
+    setValue(value);
+  };
 
-  const renderElement = useCallback(props => {
+  const renderElement = useCallback((props) => {
     switch (props.element.type) {
-      case 'code':
-        return <CodeElement {...props} />
+      case "code":
+        return <CodeElement {...props} />;
       default:
-        return <DefaultElement {...props} />
+        return <DefaultElement {...props} />;
     }
-  }, [])
+  }, []);
 
-  const renderLeaf = useCallback(props => {
-    return <Leaf {...props} />
-  }, [])
-
+  const renderLeaf = useCallback((props) => {
+    return <Leaf {...props} />;
+  }, []);
 
   return (
     <div className={styles.editor}>
       <Slate editor={props.editor} value={value} onChange={handleChange}>
-        <Editable renderElement={renderElement} renderLeaf={renderLeaf}/>
+        <Editable renderElement={renderElement} renderLeaf={renderLeaf} />
       </Slate>
     </div>
-  )
-}
+  );
+};
 
-export default Editor
+export default Editor;
